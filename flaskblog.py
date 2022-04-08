50f0b11bc432ef3d6e09338eed729303c33cd631
harshpoddar2003 <harshp1605@gmail.com>
2022-04-08 14:26:41 +0530

    added style

--- flaskblog.py
+++ flaskblog.py
@@ -1,15 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template,url_for
+from matplotlib.pyplot import title
 
 app = Flask(__name__)
+
+
+
+posts = [
+
+    {
+        'author' : 'harsh',
+        'title'  : 'Blog post 1',
+        'content': ' First post content',
+        'date_posted' : ' 1st may 2022'
+
+    }
+
+
+]
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h>Home</h>"
+    return render_template('home.html',posts=posts)
 
 @app.route("/about")
 def about():
-    return "<p>About page</p>"
+    return render_template('about.html',title='About')
 
 
 if __name__ == '__main__' :
