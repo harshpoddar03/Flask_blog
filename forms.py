0a09a3dbae25dee7b15dc0c4ce89120acbe8cbc5
harshpoddar2003 <harshp1605@gmail.com>
2022-04-09 07:02:09 +0530

    login and register

---
+++ forms.py
@@ -0,0 +1,23 @@
+
+import sys
+sys.path.insert(1, '/users/podda/anaconda3/lib/site-packages')
+from wsgiref.validate import validator
+from flask_wtf import FlaskForm
+from wtforms import StringField , PasswordField , SubmitField , BooleanField
+from wtforms.validators import DataRequired, Length , Email , EqualTo
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username',validators=[DataRequired(),Length(min=2,max =20)])
+    email = StringField('Email',validators=[DataRequired(),Email()])
+    password = PasswordField('Password',validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email',validators=[DataRequired(),Email()])
+    password = PasswordField('Password',validators=[DataRequired()])
+    remember =BooleanField('Remember Me')
+    submit = SubmitField('Login')
+
+
